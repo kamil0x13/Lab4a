9c7bad0220439efe30c460712235ff9bd0eaf169
Kamil Pietrzak <pietrzakkamil013@gmail.com>
2022-06-18 20:01:55 +0200

    Klasa TestFileRead

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
@@ -0,0 +1,19 @@
+package pl.lublin.wsei.java.cwiczenia.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestFileRead {
+    public static void main(String[] args) {
+        try{
+            String contents = new String(Files.readAllBytes(Paths.get("gusInfoGraphic.xml")));
+            System.out.println("ZAWARTOŚĆ PLIKU gusInfoGraphic.xml");
+            System.out.println(contents);
+        }catch (IOException e){
+            System.out.println("Błąd wczytywania pliku => " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+}
