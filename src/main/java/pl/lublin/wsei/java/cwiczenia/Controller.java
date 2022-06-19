42f56f8835ebf177c04fca5bdfe4828bc2da5fd8
Kamil Pietrzak <pietrzakkamil013@gmail.com>
2022-06-19 10:47:04 +0200

    controller btnOpenFileAction

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,14 +1,34 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label lbFile;
+
+    @FXML
+    public void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    @FXML
+    public void btnOpenFileAction(ActionEvent event) {
+        File file = fileChooser.showOpenDialog(null);
+        if (file!= null) {
+            lbFile.setText(file.getAbsolutePath());
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik ...");
+
+        }
     }
 }
