1b9c55fcca040231cd07913aa09bc0d1f398594d
Kamil Pietrzak <pietrzakkamil013@gmail.com>
2022-06-19 12:13:05 +0200

    ListView

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,8 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -14,6 +17,11 @@ public class Controller {
 
     @FXML
     private Label lbFile;
+    @FXML
+    private ListView lstInfografiki;
+
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
 
     @FXML
     public void initialize(){
@@ -24,7 +32,10 @@ public class Controller {
     public void btnOpenFileAction(ActionEvent event) {
         File file = fileChooser.showOpenDialog(null);
         if (file!= null) {
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
         }
         else {
             lbFile.setText("Proszę wczytać plik ...");
